a7076eb221ef1291c648c66a901ca44b931a513a
ramesh <rameshricky24@gmail.com>
2023-04-05 13:16:35 +0530

    query

--- new.js
+++ new.js
@@ -1,6 +1,13 @@
 console.log('Hello world'); 
 document.title = 'Hello'
 
-var head1 = document.getElementsByClassName('list-group-item')
-console.log(head1)
-head1[2].style.fontWeight = 'bold'
+// var head1 = document.getElementsByClassName('list-group-item')
+// console.log(head1)
+// head1[2].style.fontWeight = 'bold'
+
+var a = document.querySelector('#header-title') ;
+console.log(a.textContent)
+
+var b = document.querySelectorAll('.list-group-item:nth-child(2)') ;
+
+console.log(b.textContent)
